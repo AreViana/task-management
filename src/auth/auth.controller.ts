0fb7d376c2a62cb424226785a3df4470099dbe2c
Arely Viana <areviana17@gmail.com>
2020-07-09 22:51:54 -0600

    ✨ Add SignUp endpoint

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body, ValidationPipe, UsePipes } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  @UsePipes(ValidationPipe)
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+}
