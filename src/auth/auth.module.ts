7b6578025f07f27d1d1ef4bbdc8ef2ad7ec033c9
Arely Viana <areviana17@gmail.com>
2020-07-10 15:21:42 -0600

    🔧 Add JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/user/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: 'my_super_secret_key',
+      signOptions: {
+        expiresIn: 3600,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from 'src/user/user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
