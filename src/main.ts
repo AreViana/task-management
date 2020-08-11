d81611215cf828fc992fd169944aae14fd7cf731
Arely Viana <areviana17@gmail.com>
2020-08-11 13:47:36 -0600

    ✨ 📝 Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,14 @@ async function bootstrap() {
 
   if (process.env.NODE_ENV == 'development') {
     app.enableCors();
+    const options = new DocumentBuilder()
+      .setTitle('Task Management API')
+      .setDescription('The API description')
+      .setVersion('1.0')
+      .addTag('enpoints')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
   } else {
     app.enableCors({ origin: process.env.ORIGIN });
     logger.log(`Accepting request from origin ${process.env.ORIGIN}`);
